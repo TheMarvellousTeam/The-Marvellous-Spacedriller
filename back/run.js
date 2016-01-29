8d7fc328d81496d7aafae05a1f9d22d0b03ad076
NiZiL <biasutto.t@gmail.com>
2016-01-29 22:52:40 +0100

    setup socket.io

--- back/run.js
+++ back/run.js
@@ -1,1 +1,11 @@
 console.log('hello')
+
+var io = require('socket.io')(1984)
+
+io.on('connection', function(socket){
+	console.log('Another fucking user incoming !')
+
+	socket.on('disconnect', function(){
+		console.log('A fucking user has been disconnected')
+	})
+})
