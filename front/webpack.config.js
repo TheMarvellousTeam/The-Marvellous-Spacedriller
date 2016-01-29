ca7b96fb09c769eeab402369c7a6e1bf88d918e5
Platane <brongniart.arthur@gmail.com>
2016-01-29 21:46:53 +0100

    :tropical_fish: kill useless webpack stuff

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -9,40 +9,18 @@ var plugins = [
     // replace keys
     new webpack.DefinePlugin({
         __PROD__: productionEnv,
-        __TEST__: testEnv,
         __VERSION__: JSON.stringify( require( './package.json' ).version ),
 
-
-        // react test process.env.NODE_ENV !== 'production' to add dev check ( not required in prod )
-        'process.env': { NODE_ENV: JSON.stringify('production') }
     }),
 
 ]
 
-if ( productionEnv )
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            },
-        })
-    )
-
-
-var entry = {}
-
-if ( testEnv )
-    entry[ 'jasmine-bundle' ] = './test/unit/jasmine-src.js'
-
-else {
-    entry[ 'bootstrap' ] = './app/scripts/directives/playlist/bootstrap.js'
-    entry[ 'ReactPlaylist' ] = './app/scripts/directives/playlist/component/app.jsx'
-
-}
 
 module.exports = {
 
-    entry: entry,
+    entry: [
+        'app' : './src/app.js'
+    ],
 
     output: {
         path: path.join(__dirname, '.tmp'),
@@ -60,31 +38,12 @@ module.exports = {
                 loader: 'babel',
                 query: {
                     cacheDirectory: true,
-                    presets: ['es2015', 'stage-0'],
-                    // plugins: ['transform-runtime'],
+                    presets: ['es2015', 'stage-2'],
                 }
             },
 
-            {
-                test: /\.jsx$/,
-                exclude: /(node_modules|\.tmp)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'stage-0', 'react'],
-                    // plugins: ['transform-runtime'],
-                }
-            },
         ]
     },
 
-    plugins: plugins,
-
-    resolve: {
-        alias: {
-            'react' :           'react/dist/react'+ ( productionEnv ? '.min.js' : '.js' ),
-            'react-dom' :       'react-dom/dist/react-dom'+ ( productionEnv ? '.min.js' : '.js' ),
-        }
-    }
 
 }
